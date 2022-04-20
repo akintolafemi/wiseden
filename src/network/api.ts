7377b0bd39e775d7036760450087961cd78dc450
akintolafemi <michealakintola106@yahoo.com>
2022-04-20 10:55:38 -0100

    fixed async storage warning

--- src/network/api.ts
+++ src/network/api.ts
@@ -5,7 +5,7 @@ import { CreateAccountRequest, CreateUserResponse, ProfileResponse, EditUserRequ
 
 import {requestClan} from './requests';
 
-const BASE_URL = 'http://68.169.57.98:83/wisedenapi/';
+const BASE_URL = 'http://68.169.57.98:83/wisedenapi/api/';
 const URL_LOGIN_USERNAME = BASE_URL + 'users/login/attempt-username-login';
 const URL_LOGIN_PASSWORD = BASE_URL + 'users/login/attempt-login';
 const URL_REGISTER = BASE_URL + 'users/save';
